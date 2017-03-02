a391b8062838716a581e230d9a5db22635b7b65d
rkarthick90 <rkarthickkumar90@icloud.com>
2017-03-02 17:14:39 +0530

    Menu Page
    This menu page contains tabs and accordion according to the content
    selction its getting collapse and shown.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,47 @@
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").addClass("toggled");
-}).blur(function(e){
-    e.preventDefault();
-    $("#wrapper").removeClass("toggled");
+$(document).ready(function(){
+    // toggle on left side
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").addClass("toggled");
+    }).blur(function(e){
+        e.preventDefault();
+        $("#wrapper").removeClass("toggled");
+    });
+
+
+    //tab content based on button click
+    $(document).off('click','.menucont [data-tab-btn]');
+    $(document).on('click','.menucont [data-tab-btn]', function(){
+        $('[data-tab-btn]').removeClass('active')
+        $(this).addClass('active');
+        var clicked = $(this).attr('data-tab-btn');
+        $('.tabcontent [data-tab-content]').fadeOut();
+        $('.tabcontent [data-tab-content="'+clicked+'"]').fadeIn();
+    });
+    
+    $(document).off('click', '[data-main-tile]');
+    $(document).on('click', '[data-main-tile]', function(){
+        var temp = $(this).attr('data-main-tile');
+        pageRedirect(temp);
+    });
+
+    $(document).off('click', '[data-sub-tile]');
+    $(document).on('click', '[data-sub-tile]', function(){
+        var temp = $(this).attr('data-sub-tile');
+        pageRedirect('detail');
+    });
 });
+
+function pageRedirect(arg){
+    switch(temp){
+        case 'menu':
+            location.href = 'pages/menu.html';
+            break;
+        case 'detail':
+            location.href = "pages/detail.html";
+            break;
+        default:
+            location.href = "/";
+            break;
+    }
+}
